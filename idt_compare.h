f5d6db7c23be68a69c14fae1834b9f70d6f37ccb
Ivan Galinskiy <ivanko.rus@gmail.com>
2010-04-14 23:57:43 -0500

    Disasm stuff added (libdisasm)

--- idt_compare.h
+++ idt_compare.h
@@ -1,3 +1,5 @@
+#include <libdis.h>
+
 typedef struct
 {
   uint16_t offset_low;
@@ -16,6 +18,32 @@ void get_idt_addresses(int32_t* buffer, idt_entry* idt)
   return;
 }
 
-void* get_function_end(void* beginning)
+void* get_function_end(char* beginning, uint32_t limit)
 {
-  
+   int pos = 0;             /* current position in buffer */
+   int size;                /* size of instruction */
+   x86_insn_t insn;         /* instruction */
+
+   x86_init(opt_none, NULL, NULL);
+
+   while (pos > BUF_SIZE)
+     {
+       /* disassemble address */
+       size = x86_disasm(beginning, limit, 0, pos, &insn);
+       if ( size )
+         {
+           /* See if the instruction is 0xcf */
+           if ((*((uint8_t*)(beginning+pos)) >> 4) == 0xcf)
+             {
+               x86_cleanup();
+               return (void*)(beginning+pos);
+             }
+           else
+             pos += size;
+         }
+       else pos++;
+     }
+
+   x86_cleanup();
+   return 0;
+}
